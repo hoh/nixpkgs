f4b4c8beb5a88f32820ff3500af3e29cb8affbb6
Hugo Herter <git@hugoherter.com>
2026-05-06 23:53:29 +0200

    nixosTests.thunderbird-thunderbolt: limit to x86_64-linux

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1686,7 +1686,7 @@ in
   test-containers-bittorrent = runTest ./test-containers-bittorrent.nix;
   thanos = runTest ./thanos.nix;
   thelounge = handleTest ./thelounge.nix { };
-  thunderbird-thunderbolt = runTest ./thunderbird-thunderbolt.nix;
+  thunderbird-thunderbolt = runTestOn [ "x86_64-linux" ] ./thunderbird-thunderbolt.nix;
   tiddlywiki = runTest ./tiddlywiki.nix;
   tigervnc = handleTest ./tigervnc.nix { };
   tika = runTest ./tika.nix;
